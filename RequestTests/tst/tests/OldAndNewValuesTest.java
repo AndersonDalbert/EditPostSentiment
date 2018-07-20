1c691535ec2336574615d3deede614186b82e7c6
Anderson Dalbert <anderson.vital@ccc.ufcg.edu.br>
2018-07-20 15:28:19 -0300

    add test to check old and new sentiment values after request

---
+++ RequestTests/tst/tests/OldAndNewValuesTest.java
@@ -0,0 +1,59 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import https_service.ChangeSentimentHttpsRequest;
+import response.Response;
+
+class OldAndNewValuesTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		ChangeSentimentHttpsRequest request = new ChangeSentimentHttpsRequest();
+		request.sendPost("neutral");
+	}
+
+	@Test
+	final void testChangeToPositive() {
+		ChangeSentimentHttpsRequest changeRequest = new ChangeSentimentHttpsRequest();
+		try {
+			changeRequest.sendPost("positive");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Response response = new Response( changeRequest.getResponse() );
+		assertEquals( "neutral", response.getOldValue() );
+		assertEquals( "positive", response.getNewValue() );
+	}
+	
+	@Test
+	final void testChangeToNegative() {
+		ChangeSentimentHttpsRequest changeRequest = new ChangeSentimentHttpsRequest();
+		try {
+			changeRequest.sendPost("negative");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Response response = new Response( changeRequest.getResponse() );
+		assertEquals( "neutral", response.getOldValue() );
+		assertEquals( "negative", response.getNewValue() );
+	}
+	
+	@Test
+	final void testKeepNeutral() {
+		ChangeSentimentHttpsRequest changeRequest = new ChangeSentimentHttpsRequest();
+		try {
+			changeRequest.sendPost("neutral");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Response response = new Response( changeRequest.getResponse() );
+		assertEquals( "add-tag", response.getLastRequestField() );
+	}
+
+}
