d1afb5046f6d253874be50b197c709f901c0e33e
Anderson Dalbert <anderson.vital@ccc.ufcg.edu.br>
2018-07-20 14:14:28 -0300

    add initial test to check server requests

---
+++ RequestTests/tst/tests/ChangeSentimentsResponseCodeTest.java
@@ -0,0 +1,63 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import https_service.ChangeSentimentHttpsRequest;
+
+class ChangeSentimentsResponseCodeTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		try {
+			ChangeSentimentHttpsRequest request = new ChangeSentimentHttpsRequest();
+			request.sendPost("neutral");
+			assertEquals(200, request.getResponseCode());
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	final void testChangeToPositive() {
+		try {
+			ChangeSentimentHttpsRequest request = new ChangeSentimentHttpsRequest();
+			request.sendPost("positive");
+			assertEquals(200, request.getResponseCode());
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	final void testChangeToNegative() {
+		try {
+			ChangeSentimentHttpsRequest request = new ChangeSentimentHttpsRequest();
+			request.sendPost("negative");
+			assertEquals(200, request.getResponseCode());
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	final void testKeepNeutral() {
+		try {
+			ChangeSentimentHttpsRequest request = new ChangeSentimentHttpsRequest();
+			request.sendPost("neutral");
+			assertEquals(200, request.getResponseCode());
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+}
